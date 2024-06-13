91278728cb0e0f6db008614f1213965b2f0e682b
Nadavushka <nadavushkadev@gmail.com>
2024-06-13 08:40:55 +0300

    change of keys

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
 
@@ -9,5 +13,5 @@ vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-n>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-m>', ':wincmd l<CR>')
